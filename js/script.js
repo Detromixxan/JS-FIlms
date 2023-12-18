2c204b0b544ba0f3cee7bcbcdcd679027e2cd587
Detromixxan <detromixxan@gmail.com>
2023-12-18 16:51:16 +0300

    'Add second version of the app'

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
-"use strict";
+let numberOfFilms
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?","");
+function start() {
+	numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели? ', '')
+
+	while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+		numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели? ', '')
+	}
+}
+
+start()
 
 const personalMovieDB = {
 	count: numberOfFilms,
@@ -8,15 +16,50 @@ const personalMovieDB = {
 	actors: {},
 	genres: [],
 	privat: false,
-};
+}
 
-const a = prompt("Один из последних просмотренных фильмов?",""),
-	  b = prompt("На сколько оцените его?", ""),
-	  c = prompt("Один из последних просмотренных фильмов?",""),
-	  d = prompt("На сколько оцените его?", "");
+function rememberMyFilms() {
+	for (let i = 0; i < 2; i++) {
+		const a = prompt('Один из полследних просмотренных фильмов?', '').trim(),
+			b = +prompt('На скольуо оцените его?', '')
+		if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+			personalMovieDB.movies[a] = b
+			console.log('Done!')
+		} else {
+			console.log('Eror!')
+			i--
+		}
+	}
+}
 
+rememberMyFilms()
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+function detectPersonaLevel() {
+	if (personalMovieDB.count < 10) {
+		console.log('Промотренно довольно мало фльмов!')
+	} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+		console.log('Вы классический зритель!')
+	} else if (personalMovieDB.count >= 30) {
+		console.log('Вы киноман!')
+	} else {
+		console.log('Ошибка!')
+	}
+}
 
-console.log(personalMovieDB);
+function writeYourGenres() {
+	for (let i = 1; i <= 3; i++) {
+		personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}?`)
+	}
+}
+
+writeYourGenres()
+
+detectPersonaLevel()
+
+function showMyDB(hidden) {
+	if (!hidden) {
+		console.log(personalMovieDB)
+	}
+}
+
+showMyDB(personalMovieDB.privat)
